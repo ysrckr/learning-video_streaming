51d1c980669eebf06b42b383e082357cb23322aa
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-11 01:38:25 +0300

    Add video URL fetching and suspense fallback

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css';
 
+import { Suspense, createResource } from 'solid-js';
+
+const getVideoURL = async () => {
+  try {
+    const res = await fetch(
+      'http://localhost:8000/videos?video_name=video.mp4',
+    );
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 function App() {
+  const [url] = createResource(getVideoURL);
+
+  console.log(url(), url.loading);
+
   return (
     <main>
-      <video controls width={500}>
-        <source src="http://localhost:8000/videos" type="video/mp4" />{' '}
-        <p>Your browser cannot play the provided video file.</p>
-      </video>
+      <Suspense fallback={<p>Loading</p>}>
+        <video controls width={500}>
+          <source src={url()?.['video_url']} type="video/mp4" />{' '}
+          <p>Your browser cannot play the provided video file.</p>
+        </video>
+      </Suspense>
     </main>
   );
 }
