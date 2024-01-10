31e3cea6e7be818e57de6178d3ef877e0c53cabf
Yasar Cakir <yasar.cakir@mirsis.com.tr>
2024-01-10 22:20:00 +0300

    Update video player width in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <main>
-      <video controls autoplay>
+      <video controls width={500}>
         <source src="http://localhost:8000/videos" type="video/mp4" />{' '}
         <p>Your browser cannot play the provided video file.</p>
       </video>
